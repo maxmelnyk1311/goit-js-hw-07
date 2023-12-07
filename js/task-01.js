49f933e4986449b46f302cfe1fbbe0f1ccdc0823
maxmelnyk1311 <141562499+maxmelnyk1311@users.noreply.github.com>
2023-12-07 21:43:55 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 const arr = document.querySelectorAll(".item");
 
 console.log(`Number of categories: ${arr.length}`);
-console.log(arr);
+
 
 arr.forEach(function (item, index) {
     console.log(`Category: ${item.querySelector("h2").textContent}`);
